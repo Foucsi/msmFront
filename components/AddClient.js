6b182637b5366111b64fbc910827576858a60d3a
Foucsi <julien.foucartreact@gmail.com>
2023-02-02 19:43:01 +0100

    fetch route post devis cote front

--- components/AddClient.js
+++ components/AddClient.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function AddClient() {
+  const [nom, setNom] = useState("");
+  const [portable, setPortable] = useState("");
+  const [email, setEmail] = useState("");
+  const [adresse, setAdresse] = useState("");
+
+  const users = useSelector((state) => state.user.value);
+
+  const handleSubmit = async () => {
+    const res = await fetch(`http://localhost:3000/devis/${users.token}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: nom,
+        tel: portable,
+        email: email,
+        adress: adresse,
+        numero: 2,
+      }),
+    });
+    const data = await res.json();
+    console.log(data.result);
+  };
+
   return (
     <div className="flex flex-col font-montserrat text-colorText h-full w-full justify-center">
       <div className="flex w-full text-lg pb-5">
@@ -9,6 +33,8 @@ export default function AddClient() {
           className="w-2/6 border-b-2 border-borderColor pl-2"
           type="text"
           placeholder="Saisir le nom du client..."
+          value={nom}
+          onChange={(e) => setNom(e.target.value)}
         />
       </div>
       <div className="flex text-lg pb-5">
@@ -17,6 +43,8 @@ export default function AddClient() {
           className="w-2/6 border-b-2 border-borderColor pl-2"
           type="text"
           placeholder="Saisir un n° de tel. portable..."
+          value={portable}
+          onChange={(e) => setPortable(e.target.value)}
         />
       </div>
       <div className="flex text-lg pb-5">
@@ -25,6 +53,8 @@ export default function AddClient() {
           className="w-2/6 border-b-2 border-borderColor pl-2"
           type="text"
           placeholder="Saisir une adresse email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className="flex text-lg pb-5">
@@ -33,8 +63,11 @@ export default function AddClient() {
           className="w-2/6 border-b-2 border-borderColor pl-2"
           type="text"
           placeholder="Saisir une adresse..."
+          value={adresse}
+          onChange={(e) => setAdresse(e.target.value)}
         />
       </div>
+      <button onClick={() => handleSubmit()}>click</button>
     </div>
   );
 }
