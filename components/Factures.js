830020d2d9208497972bae1a5cf20436c9603d40
Foucsi <julien.foucartreact@gmail.com>
2023-02-02 10:30:24 +0100

    mis en place de la page visualisation facture coté front

--- components/Factures.js
+++ components/Factures.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { FcDocument } from "react-icons/fc";
 
 export default function Factures() {
+  const [listingFactures, setListingFactures] = useState([]);
   return (
     <div className="w-full h-full p-5">
-      <div className="w-full  p-5">
+      <div className="flex items-center justify-between w-full p-5 border-b-2 border-borderColor">
         <h1 className="text-3xl font-montserrat text-colorText">Factures</h1>
+        <button className="bg-colorBlue text-white py-3 px-5 rounded-lg hover:shadow-md font-montserrat hover:opacity-95">
+          Créer une facture
+        </button>
+      </div>
+      <div className="flex items-center justify-center w-full h-5/6 ">
+        {listingFactures.length === 0 && (
+          <div className="flex flex-col items-center justify-around h-96">
+            <FcDocument size={172} />
+            <p className="font-montserrat text-colorText text-xl">
+              Aucune facture pour le moment
+            </p>
+            <div className="flex items-center justify-center">
+              <button className="bg-colorBlue text-white py-3 px-5 rounded-lg hover:shadow-md font-montserrat hover:opacity-95">
+                Créer une facture
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
