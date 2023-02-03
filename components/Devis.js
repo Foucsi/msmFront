5d8d80f6954b0924316562860f94d3f96fc4d5b4
Foucsi <julien.foucartreact@gmail.com>
2023-02-03 08:44:05 +0100

    recup all devis route fetch front

--- components/Devis.js
+++ components/Devis.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FcCalculator } from "react-icons/fc";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
@@ -6,6 +6,27 @@ import { useRouter } from "next/router";
 export default function Devis() {
   const [listingDevis, setListingDevis] = useState([]);
   const users = useSelector((state) => state.user.value);
+
+  const fetchData = async () => {
+    const res = await fetch(`http://localhost:3000/devis/getAllDevis`);
+    const data = await res.json();
+
+    {
+      data.result && setListingDevis(data.devis);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const elmtDevis = listingDevis.map((devis, index) => {
+    return (
+      <div key={index}>
+        <p>{devis.name}</p>
+      </div>
+    );
+  });
 
   const router = useRouter();
   return (
@@ -23,7 +44,7 @@ export default function Devis() {
         )}
       </div>
       <div className="flex items-center justify-center w-full h-5/6 ">
-        {listingDevis.length === 0 && (
+        {listingDevis.length === 0 ? (
           <div className="flex flex-col items-center justify-around h-96">
             <FcCalculator size={172} />
             <p className="font-montserrat text-colorText text-xl">
@@ -41,6 +62,8 @@ export default function Devis() {
               )}
             </div>
           </div>
+        ) : (
+          elmtDevis
         )}
       </div>
     </div>
