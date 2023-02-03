2215ec118fe9ebf6c3abf84b3370a5d8a7a5ada6
Foucsi <julien.foucartreact@gmail.com>
2023-02-03 14:21:49 +0100

    update addDevis

--- components/AddClient.js
+++ components/AddClient.js
@@ -27,14 +27,13 @@ export default function AddClient({ setDisplayFormClient }) {
     if (data.result) {
       setDisplayFormClient(false);
     } else if (data.error === "Missing or empty fields") {
-      setMsgError("Veuillez saisir le nom du client!");
+      setMsgError(<p className="">Veuillez saisir le nom du client.</p>);
     }
   };
 
   return (
-    <div className="flex flex-col font-montserrat text-colorText h-full w-full justify-center pb-5">
-      <p>{msgError}</p>
-      <div className="flex w-full text-lg pb-5">
+    <div className="flex flex-col font-montserrat text-colorText h-full w-full justify-start pb-5 ">
+      <div className="flex w-full text-lg pb-5 pt-5">
         <p className="pr-5 w-1/6">Nom du client</p>
         <input
           className="w-2/6 border-b-2 border-borderColor pl-2"
@@ -74,7 +73,9 @@ export default function AddClient({ setDisplayFormClient }) {
           onChange={(e) => setAdresse(e.target.value)}
         />
       </div>
-
+      <div className="h-6 font-montserrat text-colorRed">
+        <p>{msgError}</p>
+      </div>
       <button
         className="bg-colorBlue text-white py-1 px-5 rounded-lg hover:shadow-md font-montserrat hover:opacity-95 w-36"
         onClick={() => handleSubmit()}
