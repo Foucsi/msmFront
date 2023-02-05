fe9dddd0d7ce8b0092b28c60152da5d37d644911
Foucsi <julien.foucartreact@gmail.com>
2023-02-05 12:14:00 +0100

    mise en page pdf

--- components/ApercuDevis.js
+++ components/ApercuDevis.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import PDFFile from "./PDFFile";
+import { Document, Page } from "react-pdf";
 
 export default function ApercuDevis({ user }) {
   return (
-    <div>
+    <div className="flex flex-col h-screen items-center p-5 justify-between">
       <PDFDownloadLink document={<PDFFile user={user} />} fileName="FORM">
         {({ loading }) =>
           loading ? (
             <button>Loading document</button>
           ) : (
-            <button>Download</button>
+            <button className="pb-2">Download</button>
           )
         }
       </PDFDownloadLink>
-
-      <p>Ici le rendu pdf</p>
+      <div className="w-3/5 h-full shadow-2xl">
+        <p></p>
+      </div>
     </div>
   );
 }
