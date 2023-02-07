0449e98b8b7004823175c8b376e6da86e4891c8e
Foucsi <julien.foucartreact@gmail.com>
2023-02-07 18:40:53 +0100

    design generate pdf

--- components/PDFFile.js
+++ components/PDFFile.js
@@ -21,6 +21,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    borderBottomColor: "#D7D7D7",
+    borderBottomStyle: "solid",
+    borderBottomWidth: 2,
   },
 });
 
@@ -36,16 +39,77 @@ export default function PDFFile({ user, numero, client, date, adress }) {
           <View
             style={{ display: "flex", flexDirection: "row", paddingLeft: 20 }}
           >
-            <Text>MSM</Text>
-            <Text>Menuiserie Sur Mesure</Text>
+            <Text style={{ fontWeight: "bold", color: "#4F3A36" }}>MSM</Text>
+            <Text
+              style={{ paddingLeft: 5, fontWeight: "bold", color: "#6C5854" }}
+            >
+              Menuiserie Sur Mesure
+            </Text>
           </View>
         </View>
-        <View>
-          <Text>Créateur : {user}</Text>
-          <Text>Numéro devis : {numero}</Text>
-          <Text>Client : {client}</Text>
-          <Text>Date : {date}</Text>
-          <Text>{adress}</Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+            fontSize: 12,
+            padding: 10,
+            height: 180,
+            backgroundColor: "grey",
+            backgroundColor: "#EBE8E8",
+          }}
+        >
+          <View style={{ width: "50%", height: "100%" }}>
+            <Text>Créateur : {user}</Text>
+            <Text>Numéro devis : {numero}</Text>
+            <Text>Date : {date}</Text>
+            <Text>Client : {client}</Text>
+            <Text>{adress}</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              flexDirection: "column",
+              width: "50%",
+              height: "100%",
+            }}
+          >
+            <Text
+              style={{
+                alignSelf: "flex-end",
+                fontWeight: "bold",
+                fontSize: 18,
+                color: "#36383A",
+              }}
+            >
+              Devis
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              11, rue Alfred de Musset
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              77176 Savigny-le-temple
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>Tél : 01.64.79.73.24</Text>
+            <Text style={{ alignSelf: "flex-end" }}>N°SIREN : 440 489 516</Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              N°SIRET : 44048951600029
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              N° RCS : B 440 489 516
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              Mail : msm@msm-sarl.fr
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              Site : www.msm-sarl.fr
+            </Text>
+            <Text style={{ alignSelf: "flex-end" }}>
+              N° TVA : FR21 440 489 516
+            </Text>
+          </View>
         </View>
 
         {({ pageNumber, totalPages }) => (
