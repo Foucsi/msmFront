2d5521460d4687ddaf3a9637169fa310e335abd8
Foucsi <julien.foucartreact@gmail.com>
2023-02-08 13:28:46 +0100

    commit

--- components/CreerDevis.js
+++ components/CreerDevis.js
@@ -6,6 +6,7 @@ import AddClient from "./AddClient";
 
 export default function CreerDevis() {
   const [displayFormClient, setDisplayFormClient] = useState(false);
+  const [displayAddArticle, setDisplayAddArticle] = useState(false);
   return (
     <div className="flex w-full h-full justify-around ">
       <div className="flex flex-col h-full w-8/12 shadow-md">
@@ -32,15 +33,27 @@ export default function CreerDevis() {
         </div>
         <div
           onClick={() => setDisplayFormClient(false)}
-          className="w-full h-2/5"
+          className={`w-full ${displayAddArticle ? "h-4/5" : "h-2/5"}`}
         >
-          <div className="flex items-center w-full h-1/6 bg-colorBgWelcome pl-10 font-montserrat text-colorText">
+          <div className="flex w-full h-1/6 bg-colorBgWelcome pl-10 font-montserrat text-colorText">
             <p>Articles</p>
           </div>
-          <div className="flex w-full h-full items-center pl-10">
-            <div className="flex items-center cursor-pointer">
+          <div className="flex flex-col justify-end w-full h-4/5 pl-10 ">
+            {displayAddArticle && (
+              <div className="h-3/4 w-4/5 shadow-sm">
+                <p>test</p>
+              </div>
+            )}
+            <div
+              className={`flex w-1/2 items-center cursor-pointer ${
+                displayAddArticle ? "pb-0" : "pb-10"
+              }`}
+            >
               <AiOutlinePlusCircle color="#252AFE" size={32} />
-              <p className="font-montserrat text-colorBlue pl-5">
+              <p
+                onClick={() => setDisplayAddArticle(!displayAddArticle)}
+                className="font-montserrat text-colorBlue pl-5"
+              >
                 Ajouter des articles
               </p>
             </div>
