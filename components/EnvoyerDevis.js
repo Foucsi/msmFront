50288c99c6d92355d1c404d073de034ce3030f3e
Foucsi <julien.foucartreact@gmail.com>
2023-02-08 12:56:17 +0100

    mise en place de la page design envoi devis par mail

--- components/EnvoyerDevis.js
+++ components/EnvoyerDevis.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function EnvoyerDevis() {
-  return <div>EnvoyerDevis</div>;
+  return (
+    <div className="h-full w-full p-5 ">
+      <div className="w-3/4 h-5/6 shadow-md"></div>
+    </div>
+  );
 }
