6c2efc9403143bf0114f5286c5263861cc8b81d2
Foucsi <julien.foucartreact@gmail.com>
2023-02-16 21:42:33 +0100

    commit

--- components/Slide2.js
+++ components/Slide2.js
@@ -4,10 +4,46 @@ import { useSelector } from "react-redux";
 export default function Slide2({ position }) {
   const users = useSelector((state) => state.user.value);
 
-  users.articles.map((e) => console.log(e.article.article));
+  const article = users.articles.map((e, index) => {
+    const articleTypes = {
+      Caissons: () => (
+        <>
+          <p>Long : {e.article.longueur}</p>
+          <p>Larg : {e.article.largeur}</p>
+          <p>Prof: {e.article.profondeur}</p>
+        </>
+      ),
+      Panneaux: () => (
+        <>
+          <p>Long : {e.article.longueur}</p>
+          <p>Larg : {e.article.largeur}</p>
+          <p>Ep: {e.article.epaisseur}</p>
+        </>
+      ),
+      Habillages: () => (
+        <>
+          <p>Haut : {e.article.hauteur}</p>
+          <p>Larg : {e.article.largeur}</p>
+          <p>Ep: {e.article.epaisseur}</p>
+        </>
+      ),
+    };
+
+    const renderArticle = articleTypes[e.article.article];
+
+    if (renderArticle) {
+      return (
+        <div className="border-2 mt-5 p-5" key={index}>
+          <p className="font-bold">{e.article.article}</p>
+          {renderArticle()}
+        </div>
+      );
+    }
+
+    return null;
+  });
 
   const slideStyle = {
-    backgroundColor: "green",
     color: "#333",
     padding: "1rem",
     borderRadius: "0.5rem",
@@ -18,11 +54,10 @@ export default function Slide2({ position }) {
         position === 1
           ? "opacity-100"
           : "opacity-0 transition-opacity duration-1000 ease-in-out"
-      }`}
+      } w-full h-full`}
       style={slideStyle}
     >
-      <p>Contenu : {users.username}</p>
-      <p>Article : </p>
+      <div className="">{article}</div>
     </div>
   );
 }
